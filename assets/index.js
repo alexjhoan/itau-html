55a25b7cbe61d9e308b0b965339f2ae187741eed
alexjhoan <alexjhoan.25@gmail.com>
2022-08-30 19:48:17 -0400

    add dropdown to navbar 2000 30082022

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,38 @@
-$(window).on('load', function () {
-  $('body').css('opacity', '1');
+$(window).on("load", function () {
+  $("body").css("opacity", "1");
 });
 
-let offset
+let offset;
 
-if (screen.width > 768){
-  offset = 200
+if (screen.width > 768) {
+  offset = 200;
 } else {
-  offset = 0
+  offset = 0;
 }
 
-new WOW({offset:offset, scrollContainer: null}).init()
+new WOW({ offset: offset, scrollContainer: null }).init();
 
-$('header').load('components/header.html')
-$('footer').load('components/footer.html')
+$("header").load("components/header.html");
+$("footer").load("components/footer.html");
+
+/*-------------------------------Nav-Bar----------------------------------*/
+
+const navDropdownShow = () => {
+  $(".nav-dropdown").addClass("opened");
+  $(".link-dropdown").addClass("opened");
+};
+
+const navDropdownHide = () => {
+  $(".nav-dropdown").removeClass("opened");
+  $(".link-dropdown").removeClass("opened");
+};
 
 /*-------------------------------Banner----------------------------------*/
-const swiperbanner = new Swiper('.gallery-banner', {
+const swiperbanner = new Swiper(".gallery-banner", {
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
