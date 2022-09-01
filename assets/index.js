6563035803fa7b1ebae2d7c0eb78ee97860b3836
anaismiraira <mirairaanais@gmail.com>
2022-09-01 14:34:43 -0400

    1434 01092022 opcions

--- assets/index.js
+++ assets/index.js
@@ -49,3 +49,31 @@ const swiperbanner = new Swiper(".gallery-banner", {
     clickable: true,
   },
 });
+/*-------------------------------Opcions----------------------------------*/
+const swiperopcions = new Swiper(".slider-opcions", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+  "320": {
+    slidesPerView: 1,
+    spaceBetween: 10,
+  },
+  "768": {
+    slidesPerView: 2,
+    spaceBetween: 20,
+  },
+  "992": {
+    slidesPerView: 3,
+    spaceBetween: 40,
+  },
+},
+});
